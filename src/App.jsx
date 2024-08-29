12f7cff6ee955777b35aa12e17ee2a6be1fc9f25
joe_2m <josseymunyi@gmail.com>
2024-08-29 14:19:00 +0300

    done files, router, link component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-function App() {
-  return (
-    <div>App</div>
-  )
-}
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
 
-export default App
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+  // <>
+  // <NavBar/>
+  // <Hero />
+  // <HomeCards/>
+  // <JbbListings />
+  // <ViewAllJobs/>
+
+  // </>
+};
+
+export default App;
